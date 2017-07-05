2421bda76bc03a75b2c5c6b538fa1e59ad228dbf
Daniel Godines <danielgodinez123@yahoo.com>
2017-07-04 17:45:28 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="BlackbodyFit",
+    version="0.1",
+    description="Curve fitting planck curve",
+    author="Daniel Godines Alcantara",
+    author_email="dg7541@bard.edu",
+    url="https://github.com/dg7541/BlackbodyFit",
+    packages=find_packages('.'),
+)
